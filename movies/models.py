fc82e830218532edf0a596fcb0d6265b9cc554c8
Andres Lopez <profesor.de.ingles@aol.com>
2023-01-17 01:23:07 -0500

    ADDED movie model and logic

---
+++ movies/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+# import uuid
+# import os
+
+
+# def poster_file_path(instance, filename):
+#     """Generate file path for movie poster."""
+#     ext = os.path.splitext(filename)[1]
+#     filename = f'{uuid.uuid4()}{ext}'
+
+#     return os.path.join('uploads', 'recipe', filename)
+
+
+class Movie(models.Model):
+    """Movie object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    score = models.DecimalField(max_digits=2, decimal_places=1)
+    description = models.TextField(blank=True)
+    review = models.TextField(blank=True)
+    # poster = models.ImageField(null=True, upload_to=poster_file_path)
+
+    def __str__(self):
+        return self.title
---
+++ movies/tests/test_movie_api.py
@@ -0,0 +1,80 @@
+"""
+Tests for recipe APIs.
+"""
+from decimal import Decimal
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from movies.models import Movie
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from movie.serializers import MovieSerializer
+
+MOVIES_URL = "http://127.0.0.1:8000/api/movie"
+# TODO: check user creation url and response once it is live in AWS
+
+def create_user(email='user@example.com', password='Testpassword!'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+def create_movie(user, **params):
+    """Create and return a sample movie."""
+    defaults = {
+        'title': 'Avatar',
+        'score': Decimal('8.3'),
+        'description': 'Sample description',
+        'review': 'Sample review',
+    }
+    defaults.update(params)
+
+    movie = Movie.objects.create(user=user, **defaults)
+    return movie
+
+
+class PublicMovieAPITests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required to call API."""
+        res = self.client.get(MOVIES_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateMovieApiTests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(email='user@example.com', password='Testpassword!')
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_movies(self):
+        """Test retrieving a list of movies."""
+        create_movie(user=self.user)
+        create_movie(user=self.user)
+
+        res = self.client.get(MOVIES_URL)
+
+        movies = Movie.objects.all().order_by('-id')
+        serializer = MovieSerializer(movies, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_movie_list_limited_to_user(self):
+        """Test list of movies is limited to authenticated user."""
+        other_user = create_user(email='other@example.com', password='Testpassword!')
+        create_movie(user=other_user)
+        create_movie(user=self.user)
+
+        res = self.client.get(MOVIES_URL)
+
+        movies = Movie.objects.filter(user=self.user)
+        serializer = MovieSerializer(movies, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
