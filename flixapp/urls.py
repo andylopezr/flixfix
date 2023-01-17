c9df42289889b1d0ef4b594c7d62e1d7c3e19459
root <root@ASUS-Andy>
2023-01-16 20:22:27 -0500

    ADDED authentication and Auth keys

--- flixapp/urls.py
+++ flixapp/urls.py
@@ -5,15 +5,15 @@ from ninja.security import HttpBearer
 from pydantic import SecretStr
 from django.contrib.auth.hashers import check_password
 from user.models import User
-from datetime import timedelta
-import jwt
+from datetime import timedelta, datetime
 from jwt import encode, PyJWTError, decode
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 
 
 class TokenPayload(Schema):
-    user_id: int=None
+    user_id: int = None
+
 
 class AuthBearer(HttpBearer):
     def authenticate(self, request, token: str) -> User:
@@ -25,8 +25,11 @@ class AuthBearer(HttpBearer):
     def get_current_user(token: str) -> User | None:
         """Check auth user"""
         try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-            token_data = TokenPayload(**payload)
+            payload = decode(
+                token,
+                settings.SECRET_KEY,
+                algorithms=['HS256'])
+
         except PyJWTError:
             return None
         user = get_object_or_404(User, email=payload['sub'])
@@ -41,19 +44,21 @@ api = NinjaAPI(
 
 
 class UserSchema(Schema):
-    email:str
-    password:str
+    email: str
+    password: str
+
 
 class LoginSchema(Schema):
-    email:str
-    password:SecretStr
+    email: str
+    password: SecretStr
+
 
 class AccessToken:
     @staticmethod
     def create(user: User) -> dict:
         email = user.email
         access_token_expires = timedelta(minutes=999999)
-        token = AccessToken.create_secure_token(
+        token = AccessToken.create_token(
             data={"sub": email},
             expires_delta=access_token_expires,
         )
@@ -63,15 +68,58 @@ class AccessToken:
             "access_token": token,
         }
 
+    @staticmethod
+    def create_token(data: dict, expires_delta: timedelta = None) -> str:
+        to_encode = data.copy()
+        expire = datetime.utcnow() + expires_delta
+        to_encode.update({"exp": expire})
+        encoded_jwt = encode(
+            to_encode, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return encoded_jwt
 
+
+# Django Ninja routes -------------------------------------------------------
+
+# User creation
 @api.post('/create-user', auth=None)
-def create_user_api(request, payload:UserSchema):
-    """Create User"""
+def create_user_api(request, payload: UserSchema):
+    """
+        Create a new user using email and password.
+
+        Password constrains:
+
+            - At least 10 characters long.
+
+            - Should include one lowercase letter.
+
+            - Should include one UPPERCASE letter.
+
+            - Should include one of these special characters: ! @ # ? ]
+
+    """
     user = User.objects.create_user(
         payload.email,
         payload.password,
     )
-    return {"id": user.id}   
+    return {"id": user.id}
+
+
+# Login
+@api.post('/login', auth=None)
+def user_login(request, payload: LoginSchema):
+    """Login using email and password"""
+    try:
+        user = User.objects.get(email=payload.email)
+
+    except Exception:
+        return "User not found!"
+
+    if check_password(payload.password.get_secret_value(), user.password):
+        return AccessToken.create(user)
+
+# Django routes ------------------------------------------------------------
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
